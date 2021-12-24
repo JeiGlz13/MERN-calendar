268cf1bedafc5d42b4aad5cf2dd84bf7b4cfc10e
Jeisson Gonzalez <jeiglz13@gmail.com>
2021-12-24 17:05:36 -0600

    initializing app

---
+++ src/router/AppRouter.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+import { LoginScreen } from '../components/auth/LoginScreen';
+import { CalendarScreen } from '../components/calendar/CalendarScreen';
+
+export const AppRouter = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    <CalendarScreen/>
+                </Route>
+                <Route exact path="/Login">
+                    <LoginScreen/>
+                </Route>
+
+                <Redirect to="/" />
+            </Switch>
+        </Router>
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+	darkMode: false, // or 'media' or 'class'
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"
+  ],
+  theme: {
+    screens: {
+			xs: "300px",
+			sm: "640px",
+			md: "768px",
+			lg: "1024px",
+			xl: "1280px",
+			"2xl": "1536px",
+		},
+    extend: {
+      fontFamily: {
+				Poppins: ["Poppins"],
+				Montserrat: ["Montserrat"],
+			},
+    },
+  },
+  plugins: [],
+}
